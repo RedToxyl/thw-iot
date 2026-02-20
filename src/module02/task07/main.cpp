9f2ca81c1da499dde8b32e89921f52e3a0e7eabe
tw <wolf19@uni-potsdam.de>
2026-02-20 16:30:02 +0200

    added relay code

---
+++ src/module02/task07/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+#define BTN D1
+#define RELEY D6
+
+boolean btn = false;
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(RELEY, OUTPUT);
+    pinMode(BTN, INPUT_PULLUP);
+
+    digitalWrite(RELEY, LOW);
+
+    Serial.flush();
+}
+
+void reley()
+{
+    digitalWrite(RELEY, HIGH);
+    delay(300);
+    digitalWrite(RELEY, LOW);
+    }
+
+void loop()
+{
+    // ===== loop version =====
+    // reley();
+    // delay(5000);
+
+    // ===== btn version =====
+    unsigned long t = millis();
+    boolean v = digitalRead(BTN) == HIGH;
+    if (btn != v){
+        btn = v;
+        Serial.printf("[%lu] Button is %s\n", t, btn ? "HIGH" : "LOW");
+        if (v){
+            reley();
+        }
+    }
+}
